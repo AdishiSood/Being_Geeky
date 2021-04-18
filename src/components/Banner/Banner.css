96a7b5ade0f277fd69568cf928a46c7ab359c0ff
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-04-18 11:12:35 +0530

    Create Banner.css

---
+++ src/components/Banner/Banner.css
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <div className="banner">
+            <h2>Lectures available anywhere anytime.</h2>
+            <p>You can watch the lecture anytime from anywhere and ask doubts.The phrasal sequence of the Lorem Ipsum text is now so widespread and commonplace that many DTP programmes can generate dummy text using the starting sequence "Lorem ipsum". Fortunately, the phrase 'Lorem Ipsum' is now recognized by electronic pre-press systems and, when found, an alarm can be raised. This avoids a publication going to print with overlooked dummy text.</p>
+        </div>
+    );
+}
+
+export default Banner;
