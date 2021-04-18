06771c4ab053846a2c144560a0b7914d3c7a16a4
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-04-19 00:10:19 +0530

    Create Footer.js

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+
+            <div>
+                <h3> Address </h3>
+                <ul>
+                    <li> ZZZZ Workspace </li>
+                    <li> Street 43,building 4A </li>
+                    <li> Industrial Area </li>
+                    <li> Aqwertyu </li>
+                    <li> Mumbai </li>
+                </ul>
+            </div>    
+
+            <div>
+                <h3> Clients </h3>
+                <ul>
+                    <li> XYZ org </li>
+                    <li> ABC org </li>
+                    <li> IJK org </li>
+                    <li> Qwerty Corp </li>
+                    <li> ZAQ PVT </li>
+                </ul>
+            </div>
+
+            <div>
+                <h3> Stack </h3>
+                <ul>
+                    <li> HTML5,CSS,JS </li>
+                    <li> React </li>
+                    <li> Angular </li>
+                    <li> NodeJS </li>
+                    <li> Django </li>
+                </ul>
+            </div>
+
+            <div>
+                <h3> About Being Geeky </h3>
+                <ul>
+                    <li> About CEO </li>
+                    <li> About Team </li>
+                    <li> Location </li>
+                    <li> Working Atmosphere </li>
+                    <li> Contact Being Geeky </li>
+                </ul>
+            </div>
+
+        </div>
+    );
+}
+
+export default Footer;
