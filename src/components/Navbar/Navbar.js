4354969dc582118787b2f05193e0c0c949b965fd
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-04-19 00:14:31 +0530

    Create Navbar.js

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Navbar.css';
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#main">Being Geeky</a></li>
+                <li><a href="#tut">Tutorials</a></li>
+                <li><a href="#video">Video</a></li>
+                <li><a href="#coup">Coupons</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
