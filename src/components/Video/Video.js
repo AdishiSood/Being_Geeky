c63b8517eb34febd495e7e7cc8390d1b7ce181c2
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-04-20 16:45:57 +0530

    Create Video.js

---
+++ src/components/Video/Video.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Video.css';
+
+const Video = () => {
+    return (
+        <div className="video" id="video">
+            <h2>The video lecture you shouldn't miss</h2>
+            <p>With one of the best content available online.And one of the best instructors it makes Being Geeky one of the best available learing platform available.he spread of computers and layout programmes thus made dummy text better known. While in earlier times several lines of the Lorem Ipsum text were repeated in the creation of dummy texts, today the full text of Cicero's 'De finibus' serves as the basis for many dummy text or Lorem Ipsum generators. Based on 'De finibus', these generators automatically create longer sections of the Lorem Ipsum text or various other filler texts.</p>
+            <button type="button">Read More</button>
+            <img src="https://beinggeeky-hnip75cdkgvvk6vt.web.codequotient.com/public/lecture.jpg" alt="img" />
+            
+        </div>
+    );
+}
+
+export default Video;
